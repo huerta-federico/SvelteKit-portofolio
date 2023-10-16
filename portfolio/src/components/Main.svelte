7afd50d1e70270371d6a9a67a9d784b15d96b92f
huerta-federico <65100237+huerta-federico@users.noreply.github.com>
2023-10-15 20:18:44 -0500

    finished benefits and added table of goodies

--- portfolio/src/components/Main.svelte
+++ portfolio/src/components/Main.svelte
@@ -1,14 +1,47 @@
 <script>
     import Step from "./Step.svelte";
 
+    // Projects Array
     let steps = [
-        {name: "Project 1", icon: "fa-brands fa-linux", description:""},
-        {name: "Project 2", icon: "", description: ""},
-        {name: "Project 3", icon: "", description: ""},
+        {
+            name: "Project 1",
+            icon: "fa-brands fa-linux",
+            description: "project description 1",
+        },
+        {
+            name: "Project 2",
+            icon: "fa-brands fa-linux",
+            description: "project description 2",
+        },
+        {
+            name: "Project 3",
+            icon: "fa-brands fa-linux",
+            description: "project description 3",
+        },
+    ];
+
+    // Benefits Array
+    let benefits = [
+        {
+            name: "Benefit 1",
+            icon: "fa-brands fa-linux",
+            description: "the best DPS in PC NA",
+        },
+        {
+            name: "Benefit 2",
+            icon: "fa-brands fa-linux",
+            description: "an excelent cookie giver",
+        },
+        {
+            name: "Benefit 3",
+            icon: "fa-brands fa-linux",
+            description: "a great passive listener",
+        },
     ];
 </script>
 
 <main class="flex flex-col flex-1 p-4">
+    <!-- Intro section -->
     <section
         id="introPage"
         class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
@@ -51,6 +84,8 @@
             />
         </div>
     </section>
+
+    <!-- Project section -->
     <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
         <div class="flex flex-col gap-2 text-center">
             <h6 class="text-large sm:text-xl md:textxl">A few of my stuff</h6>
@@ -66,13 +101,133 @@
             <i class="fa-regular fa-circle-play" />
             <p>Watch the video</p>
         </a>
+
+        <!-- Project containers -->
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
             <Step step={steps[0]}>
-                <p>Project 1 is just a placeholder for the description of stuff, things and stuff.</p>
+                <p>
+                    Project 1 is just a placeholder for the description of
+                    stuff, things and <strong class="text-violet-400"
+                        >specially the super important stuff</strong
+                    > with some added detail at the end.
+                </p>
             </Step>
-            <Step step={steps[1]}></Step>
-            <Step step={steps[2]}></Step>
+            <Step step={steps[1]}>
+                <p>
+                    Project 2 is just a placeholder for the description of
+                    stuff, things and <strong class="text-violet-400"
+                        >specially the super important stuff</strong
+                    > with some added detail at the end.
+                </p></Step
+            >
+            <Step step={steps[2]}>
+                <p>
+                    Project 3 is just a placeholder for the description of
+                    stuff, things and <strong class="text-violet-400"
+                        >specially the super important stuff</strong
+                    > with some added detail at the end.
+                </p></Step
+            >
+        </div>
+    </section>
+
+    <!-- About section -->
+    <section
+        id="about"
+        class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+    >
+        <div
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+        >
+            <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> me.
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+            I am . . .
+        </p>
+
+        <!-- Benefits containers -->
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+                <div class="flex gap-6 sm:gap-8">
+                    <p
+                        class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                    >
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
         </div>
 
+        <!-- Table of goodies -->
+        <h5>The <span class="text-violet-400"> Complete</span> Package</h5>
+        <div
+            class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+        >
+            <table class="bg-white text-slate-700 rounded text-center">
+                <thead class={"border-b border-solid border-slate-200  "}>
+                    <tr class="">
+                        <th />
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+                        <th
+                            class="bg-violet-700 text-white whitespace-nowrap p-4 px-8"
+                            >Me</th
+                        >
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Dedication</td
+                        >
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Critical Thought</td
+                        >
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr>
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Interpersonal Skills</td
+                        >
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr class="border-t border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Progamming Ability</td
+                        >
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </section>
 </main>
